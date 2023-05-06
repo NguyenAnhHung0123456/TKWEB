641764803d3d3564086e4db3f64176bc1e7a07db
Nguyễn Anh Hùng <119340434+NguyenAnhHung0123456@users.noreply.github.com>
2023-05-06 09:01:10 +0700

    Update main.js

--- Bai02/EX03/main.js
+++ Bai02/EX03/main.js
@@ -12,22 +12,5 @@ for (let i = 0; i < trangChu.length; i++) {
 
         content[i].classList.add('active')
         trangChu[i].classList.add('enable')
-
-        const introduceElement = document.querySelector('.body__container-introduce')
-        if(e.target.innerText === 'Trang chủ') {
-            document.title = 'Nguyễn Hùng Web Design | Trang chủ'
-
-            introduceElement.style.display = 'block'
-        } else {
-            introduceElement.style.display = 'none'
-        }
-
-        if(e.target.innerText === 'Thông tin') {
-            document.title = 'Nguyễn Hùng Web Design | Thông tin'
-        }
-
-        if(e.target.innerText === 'Dịch vụ') {
-            document.title = 'Nguyễn Hùng Web Design | Dịch vụ'
-        }
     }
 }
