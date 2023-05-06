4894b1d3726ce8aeacb35bef4e2bd73188effe0f
Nguyễn Anh Hùng <119340434+NguyenAnhHung0123456@users.noreply.github.com>
2023-05-06 09:26:00 +0700

    Update main.js

--- Bai02/EX04/main.js
+++ Bai02/EX04/main.js
@@ -1,1 +1,16 @@
+const trangChu = document.querySelectorAll('.heading__item-link')
+let content = document.querySelectorAll('.container-content')
 
+for (let i = 0; i < trangChu.length; i++) {
+
+    trangChu[i].onclick = function(e) {
+        const active = document.querySelector('.active')
+        const enable = document.querySelector('.enable')
+
+        active.classList.remove('active')
+        enable.classList.remove('enable')
+
+        content[i].classList.add('active')
+        trangChu[i].classList.add('enable')
+    }
+}
